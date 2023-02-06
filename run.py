7b686880e55bdbac9934752d3e133b5aa8e507d9
Nisha Saduagkan <nishasdk@Nishas-MacBook-Pro.local>
2023-02-06 13:22:11 +0000

    adding run file with todo
    adding run file with todo

---
+++ run.py
@@ -0,0 +1,35 @@
+# Parking Garage model calculator - main code (run.py)
+#
+# Created on Mon Feb 06 2023
+#
+# Copyright (c) 2023 N Saduagkan
+#
+
+
+''' TODO: set up demand -> deterministic AND stochastic'''
+
+''' TODO: ask for design variable input'''
+
+''' TODO: define parameters * create a class?'''
+
+''' TODO: create NPV function for initial design vector - rigid and flex'''
+
+''' TODO: plot the deterministic and stochastic demands'''
+
+''' TODO: implement decision rules'''
+
+''' TODO: optimise the rigid & flex design vectors under deterministic demand'''
+''' TODO: optimise the rigid & flex design vectors under stochastic demand'''
+# at this point it proves that optimal values will not be the same under a stochastic demand
+
+''' TODO: montecarlo simulation for n cases - generate n different demand scenarios'''
+# show uncertainty across scenarios
+
+''' TODO: set up ENPV function - rigid AND flex'''
+
+''' TODO: optimise against ENPV to find optimal design variable'''
+# prove that flexibility can 
+
+''' TODO: plot the results, histogram, cdf distribution'''
+
+''' TODO: iterative history? sensitivity analysis? multiobjective? '''
