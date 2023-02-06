0f7e8e5cc5c973d289d0ae1d49ded2008a499dbe
nishasdk <tngg.sdk@gmail.com>
2023-02-06 23:17:36 +0000

    finalised garage_demand

--- cesa_/garage_demand.py
+++ cesa_/garage_demand.py
@@ -1,122 +1,101 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  9 15:32:02 2020
+# Defining demand for multi-story parking garage
+#
+# N Saduagkan, Feb 2023
+# @nishasdk
+#
 
-@author: cesa_
-"""
 import math
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import typing
 
+## SEED ##
+seed_number = 69
 
 # Parameters
-T=20 #years
-cc = 16000# Construction cost per parking space
-cl = 3600000# Annual leasing land cost
-#p = 0.00# Percentage of construction cost to acquire flexibility (i.e. fatter columns). Note that this is set to 0 to determine how much the flexibility is worth. The real value can be found by subracting the acquiistion cost from the NPV for a particular design.
-cr = 2000# Operating cost per parking space
-#ct = []# Total construction cost
-gc = 0.10# Growth in construction cost per floor above two floors
-n0 = 200# Initial number of parking space per floor
-p = 10000# Price per parking space
-r = 0.12# Discount rate
-fmin = 2# Minimum number of floors built
-fmax = 9# Maximum number of floors built
+time_lifespan = 20  # years
+time_arr = np.array(range(time_lifespan+1))  # time array
 
-years = ['0' ,'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16','17','18','19','20']
+years = list(map(str, range(time_lifespan)))  # Array of strings for np header
+cost_construction = 16000  # Construction cost per parking space
+cost_land = 3600000  # Annual leasing land cost
+# p = 0.00# Percentage of construction cost to acquire flexibility (i.e. fatter columns). Note that this is set to 0 to determine how much the flexibility is worth. The real value can be found by subracting the acquiistion cost from the NPV for a particular design.
+cost_ops = 2000  # Operating cost per parking space
+
+growth_factor = 0.10  # Growth in construction cost per floor above two floors
+space_initial = 200  # Initial number of parking space per floor
+price = 10000  # Price per parking space
+rate_discount = 0.12  # Discount rate
+floor_min = 2  # Minimum number of floors built
+floor_max = 9  # Maximum number of floors built
 
 # Demand Variables
 
-D_1 = 750 # Projected year 1 demand
-D_10 = 750 #additional demand by year 10
-D_F= 250 # additional demand after year 10
-T = 20 # project duration in years
-alpha = D_10 + D_F; # Parameter for demand model showing difference between initial and final demand values
-beta = -math.log(D_F/alpha)/(T/2 - 1) # Parameter for demand model showing growth speed of demand curve
+demand_1 = 750  # Projected year 1 demand
+demand_10 = 750  # additional demand by year 10
+demand_final = 250  # additional demand after year 10
+
+# Stochastic demand variables
+
+off_D0 = 0.1  # Realised demand in yr 1 within "x" perccentage of demand projection
+off_D10 = 0.1  # Additional demand by year 10 within "x" percentage of demand projection
+# Additional demand after year 10 within "x" percentage of demand projection
+off_Dfinal = 0.1
+# Annual volatility of demand growth within "x" percentage of growth projection
+volatility = 0.1
 
 
-def demand_static(t): # t represents year
-        D_stat = D_1 + D_10 + D_F -alpha * math.exp(-beta*(t-1))
-        return D_stat
-    
-  
- # Stochastic demand variables
-
- 
-
-    
-def demand_stochastic(t) : 
-    #np.random.seed(7) # set consdtant seed for simulations to for standardized comparison
-    offD0 = 0.50 # Realised demand in yr 1 within "x" perccentage of demand projection
-    offD10 = 0.50 # Additional demand by year 10 within "x" percentage of demand projection
-    offDf = 0.50 # Additional demand after year 10 within "x" percentage of demand projection
-    vol = 0.15 # Annual volatility of demand growth within "x" percentage of growth projection
-    rD0 = (1-offD0)*D_1 + np.random.random_sample()*2*offD0*D_1 # Realised demand in year 0
-    rD10 = (1-offD10)*D_10 +np.random.random_sample()*2*offD10*D_10 # Realised additional demand by year 10
-    rDf = (1-offDf)*D_F + np.random.random_sample()*2*offDf*D_F# Realised additional demand after year 10
-    alpha_stoc = rD10 + rDf # Parameter for demand model showing difference between initial and final demand values
-    beta_stoc = -math.log(rDf/alpha_stoc)/(T/2 - 1)#  Parameter for demand model showing growth speed of demand curve
-    D_stoc1 = rD0 + rD10 + rDf  -alpha_stoc * math.exp(-beta_stoc *(t-1)) # projected demand vector
-    D_stoc2 = rD0 + rD10 + rDf  -alpha_stoc * math.exp(-beta_stoc *(t-2)) # projected demand vector shifted by one period to right
-    D_g_proj = (D_stoc1/D_stoc2) -1
-    R_g = D_g_proj - vol +   np.random.random_sample()*2*vol
-    D_stoc = D_stoc2 *(1 + R_g)
-    return D_stoc
-
-def demand_stochastic_series(t) : #t is years
-    #np.random.seed(7) # set consdtant seed for simulations to for standardized comparison
-    demand_projections = pd.Series(index=years)
-    rD0s = np.random.random_sample() # Realised demand in year 0
-    rD10s = np.random.random_sample() # Realised additional demand by year 10
-    rDfs =np.random.random_sample()# Realised additional demand after year 10
-    for i in range(0,t+1):
-        demand_projections[i] = demand_stochastic_less(i,rD0s,rD10s,rDfs)
-    return demand_projections
+def cost_initial(floor_initial: int) -> float:
+    return cost_construction * space_initial * ((((1 + growth_factor) ** (floor_initial - 1) - (1 + growth_factor)) / growth_factor)) + (2 * space_initial * cost_construction) if floor_initial > 2 else floor_initial * space_initial * cost_construction
 
 
+def demand_deterministic(time_arr: np.array) -> np.array:
+    # Parameter for demand model showing difference between initial and final demand values
+    alpha = demand_10 + demand_final
+    # Parameter for demand model showing growth speed of demand curve
+    beta = -np.log(demand_final / alpha) / (time_lifespan / 2 - 1)
+    return demand_1 + demand_10 + demand_final - alpha * np.exp(-beta * (time_arr - 1))
 
 
-def demand_stochastic_less(t,rD0s, rD10s, rDfs) : #this is for RL environment so random sample does not get recalculated every time
-    #np.random.seed(7) # set consdtant seed for simulations to for standardized comparison
-    offD0 = 0.50 # Realised demand in yr 1 within "x" perccentage of demand projection
-    offD10 = 0.50 # Additional demand by year 10 within "x" percentage of demand projection
-    offDf = 0.50 # Additional demand after year 10 within "x" percentage of demand projection
-    vol = 0.15 # Annual volatility of demand growth within "x" percentage of growth projection
-    rD0 = (1-offD0)*D_1 + rD0s*2*offD0*D_1 # Realised demand in year 0
-    rD10 = (1-offD10)*D_10 +rD10s*2*offD10*D_10 # Realised additional demand by year 10
-    rDf = (1-offDf)*D_F + rDfs*2*offDf*D_F# Realised additional demand after year 10
-    alpha_stoc = rD10 + rDf # Parameter for demand model showing difference between initial and final demand values
-    beta_stoc = -math.log(rDf/alpha_stoc)/(T/2 - 1)#  Parameter for demand model showing growth speed of demand curve
-    D_stoc1 = rD0 + rD10 + rDf  -alpha_stoc * math.exp(-beta_stoc *(t-1)) # projected demand vector
-    D_stoc2 = rD0 + rD10 + rDf  -alpha_stoc * math.exp(-beta_stoc *(t-2)) # projected demand vector shifted by one period to right
-    D_g_proj = (D_stoc1/D_stoc2) -1
-    R_g = D_g_proj - vol +   np.random.random_sample()*2*vol
-    D_stoc = D_stoc2 *(1 + R_g)
-    return D_stoc
+plt.figure()
+print(demand_deterministic(time_arr))
 
-def demand_stochastic_less_series(t, rD0, rD10, rDf) : #this is for RL environment so random sample does not get recalculated every time
-    #np.random.seed(7) # set consdtant seed for simulations to for standardized comparison
-    years = ['0' ,'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16','17','18','19','20']
-    demand_projections = pd.Series(index=years)
-    T=20
-    offD0 = 0.50 # Realised demand in yr 1 within "x" perccentage of demand projection
-    offD10 = 0.50 # Additional demand by year 10 within "x" percentage of demand projection
-    offDf = 0.50 # Additional demand after year 10 within "x" percentage of demand projection
-    vol = 0.15 # Annual volatility of demand growth within "x" percentage of growth projection
-    alpha_stoc = rD10 + rDf # Parameter for demand model showing difference between initial and final demand values
-    beta_stoc = -math.log(rDf/alpha_stoc)/(T/2 - 1)#  Parameter for demand model showing growth speed of demand curve
-    D_stoc1 = rD0 + rD10 + rDf  -alpha_stoc * math.exp(-beta_stoc *(t-1)) # projected demand vector
-    D_stoc2 = rD0 + rD10 + rDf  -alpha_stoc * math.exp(-beta_stoc *(t-2)) # projected demand vector shifted by one period to right
-    D_g_proj = (D_stoc1/D_stoc2) -1
-    R_g = D_g_proj - vol +   np.random.random_sample()*2*vol
-    D_stoc = D_stoc2 *(1 + R_g)
-    return D_stoc
+plt.plot(time_arr[1:], demand_deterministic(time_arr[1:]))
 
 
+def demand_stochastic(time_arr: np.array) -> np.array:
+    # set constant seed for simulations to for standardized comparison
+    np.random.seed(seed_number)
+    rD0 = round((1 - off_D0) * demand_1 + np.random.rand() *
+                2 * off_D0 * demand_1)  # Realised demand in year 0
 
-def cc_start(f0):
-    if f0 > 2:
-        cc_i = cc * n0 * ((((1+gc)**(f0-1) - (1+gc))/gc)) + (2*n0*cc)
-    else : 
-        cc_i= f0*n0*cc
-    return cc_i
+    rD10 = round((1 - off_D10) * demand_10 + np.random.rand() * 2 *
+                 off_D10 * demand_10)  # Realised additional demand by year 10
+
+    rDf = round((1 - off_Dfinal) * demand_final + np.random.rand() * 2 *
+                off_Dfinal * demand_final)  # Realised additional demand after year 10
+
+    # Parameter for demand model showing difference between initial and final demand values
+    alpha_stoc = rD10 + rDf
+
+    # Parameter for demand model showing growth speed of demand curve
+    beta_stoc = -np.log(rDf / alpha_stoc) / (time_lifespan / 2 - 1)
+
+    D_stoc1 = (rD0 + rD10 + rDf - alpha_stoc *
+               np.exp(-beta_stoc * (time_arr - 1)))  # projected demand vector
+
+    # projected demand vector shifted by one period to right
+    D_stoc2 = rD0 + rD10 + rDf - alpha_stoc * \
+        np.exp(-beta_stoc * (time_arr - 2))
+    D_g_proj = D_stoc1 / D_stoc2 - 1
+    R_g = D_g_proj - volatility + \
+        np.random.rand(len(time_arr)) * 2 * volatility
+
+    return np.multiply(D_stoc2, (1 + R_g))
+
+
+print(demand_stochastic(time_arr))
+
+plt.plot(time_arr[1:], demand_stochastic(time_arr[1:]))
+plt.show()
